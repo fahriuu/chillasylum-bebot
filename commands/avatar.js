ee8affc54f49d50016495875ac5aeffec10c1358
Gilbertt1214 <rianfahrian579@gmail.com>
2025-12-15 15:34:20 +0700

    aaab

--- commands/avatar.js
+++ commands/avatar.js
@@ -14,26 +14,12 @@ module.exports = {
     async execute(interaction) {
         const user = interaction.options.getUser("user") || interaction.user;
 
-        const formats = ["png", "jpg", "webp"];
-        if (user.avatar?.startsWith("a_")) formats.push("gif");
-
-        const links = formats
-            .map(
-                (f) =>
-                    `[${f.toUpperCase()}](${user.displayAvatarURL({
-                        extension: f,
-                        size: 1024,
-                    })})`
-            )
-            .join(" | ");
-
         const embed = new EmbedBuilder()
             .setColor("#2b2d31")
             .setAuthor({
                 name: user.tag,
                 iconURL: user.displayAvatarURL({ size: 64 }),
             })
-            .setDescription(`Download: ${links}`)
             .setImage(user.displayAvatarURL({ size: 1024, dynamic: true }))
             .setFooter({ text: `Requested by ${interaction.user.username}` })
             .setTimestamp();
